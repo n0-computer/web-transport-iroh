b8bfbe81d90a7ff22f0d30c70dc10c3222a0297d
Frando <frando@unbiskant.org>
2026-02-13 16:09:15 +0100

    fixup

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use iroh::endpoint::{Connection, QuicTransportConfig};
+use iroh::endpoint::QuicTransportConfig;
 use iroh::{EndpointAddr, endpoint::ConnectOptions};
 use url::Url;
 

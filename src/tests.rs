4d0440d38ce4dc56acb90c6e0e1bce611ea32149
Frando <frando@unbiskant.org>
2026-03-26 10:44:38 +0100

    deps: bump to iroh@main

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,7 @@
-use iroh::{Endpoint, endpoint::ConnectionError};
+use iroh::{
+    Endpoint,
+    endpoint::{ConnectionError, presets},
+};
 use n0_tracing_test::traced_test;
 use tracing::Instrument;
 use url::Url;
@@ -8,15 +11,14 @@ use crate::{ALPN_H3, Client, H3Request, QuicRequest, SessionError};
 #[tokio::test]
 #[traced_test]
 async fn h3_smoke() -> n0_error::Result<()> {
-    let client = Endpoint::empty_builder()
-        .bind()
+    let client = Endpoint::bind(presets::Minimal)
         .instrument(tracing::error_span!("client-ep"))
         .await
         .unwrap();
     let client_id = client.id();
     let client = Client::new(client);
 
-    let server = Endpoint::empty_builder()
+    let server = Endpoint::builder(presets::Minimal)
         .alpns(vec![ALPN_H3.as_bytes().to_vec()])
         .bind()
         .instrument(tracing::error_span!("server-ep"))
@@ -80,11 +82,11 @@ async fn h3_smoke() -> n0_error::Result<()> {
 async fn quic_smoke() -> n0_error::Result<()> {
     const ALPN: &str = "moql";
 
-    let client = Endpoint::empty_builder().bind().await.unwrap();
+    let client = Endpoint::bind(presets::Minimal).await.unwrap();
     let client_id = client.id();
     let client = Client::new(client);
 
-    let server = Endpoint::empty_builder()
+    let server = Endpoint::builder(presets::Minimal)
         .alpns(vec![ALPN.as_bytes().to_vec()])
         .bind()
         .await
